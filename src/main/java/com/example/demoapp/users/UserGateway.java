2870e5f1723437f259e39e1f9ac50befb58c18a0
Kongpop Suwannawong <kongpop.s@kbtg.tech>
2021-09-07 16:41:06 +0700

    using resttemplate call rest api

--- src/main/java/com/example/demoapp/users/UserGateway.java
+++ src/main/java/com/example/demoapp/users/UserGateway.java
@@ -1,11 +1,36 @@
 package com.example.demoapp.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Component
 public class UserGateway {
-    public UserResponse getUserById(*)int id){
-        return null;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+//    public UserResponse getUserById(int id){
+//        String url ="https://jsonplaceholder.typicode.com/users/"+id;
+//        UserResponse response = restTemplate.getForObject(url,UserResponse.class);
+//        if(null!=response) {
+//            return response;
+//        }
+//        throw new RuntimeException("API error");
+//    }
+
+    public Optional<UserResponse> getUserById(int id){
+        String url ="https://jsonplaceholder.typicode.com/users/"+id;
+        try {
+            UserResponse response = restTemplate.getForObject(url, UserResponse.class);
+            return Optional.ofNullable(response);
+        }catch (RestClientException e){
+            return Optional.empty();
+        }
+
     }
 
 
